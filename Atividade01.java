66b657f6bbf0d0fe8ad36511ee96e3dd16f188b5
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-08-13 16:12:37 -0300

    salvar

---
+++ Atividade01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Atividade01 {
+    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("informe o numero1: ");
+        double N1=sc.nextDouble();
+
+        System.out.println("informe o numero2: ");
+        double N2=sc.nextDouble();
+
+        System.out.println(soma);
+
+
+        
+    }
+    
+}
---
+++ Aula01.java
@@ -0,0 +1,19 @@
+//classe conjunto de atributos e metodos
+public class Aula01 {
+
+
+
+
+
+    
+    //metodo de execução
+    public static void main(String[] args) {
+        System.out.print("ola,mundo");
+        System.out.println("ola,mundo");
+        
+    
+    
+        
+    }
+    
+}
