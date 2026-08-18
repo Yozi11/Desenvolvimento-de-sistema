0aae2bcb19010fbe5ac5d09019e6d71e0f95cf9a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-08-18 16:27:58 -0300

    vetores python

---
+++ Vetores.py
@@ -0,0 +1,15 @@
+numeros = [33,11,22,54,51,52,17]
+nomes = ["vitor","matheus","daniel","Darlan"]
+
+nomes.remove("matheus")
+print(nomes)
+
+
+#append - insere um novo valor no vetor, sempre na ultima posiçao
+#insert - insere um novo valor na posiçao desejada.
+#pop - Deleta um valor pela sua posiçao
+#remove - Deleta um valor  pelo seu conteudo
+#sort
+
+
+
